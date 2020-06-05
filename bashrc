52ae626eeb17d4ee972e65328428c4780f27fcc1
Ian Fan <phantasmcode@gmail.com>
2020-06-04 23:33:07 -0700

    Various update

--- bashrc
+++ bashrc
@@ -1,3 +1,4 @@
 export PS1='\[\033[01;31m\]\h \[\033[01;32m\]\W\[\033[01;34m\]$(function _f(){ git $1 --short HEAD 2>/dev/null;}; (_f symbolic-ref || _f rev-parse) | sed -E "s/(.+)/ \1/")\[\033[01;36m\] \$ \[\033[0m\]'
+export DOTFILES_DIR=$(dirname $(readlink -f $HOME/.bashrc))
 
-source ~/.dotfiles/.alias
+source $DOTFILES_DIR/alias
--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-DOTFILE_DIR=$(dirname $(readlink -f $0))
-ln -s $DOTFILE_DIR/bashrc ~/.bashrc
-ln -s $DOTFILE_DIR/bash_profile ~/.bash_profile
-ln -s $DOTFILE_DIR/inputrc ~/.inputrc
+DOTFILES_DIR=$(dirname $(readlink -f ${BASH_SOURCE[0]}))
+ln -s $DOTFILES_DIR/bashrc ~/.bashrc
+ln -s $DOTFILES_DIR/bash_profile ~/.bash_profile
+ln -s $DOTFILES_DIR/inputrc ~/.inputrc
